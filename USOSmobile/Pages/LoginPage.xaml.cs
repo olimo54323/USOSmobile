0c9945c4b8bbfbb6047a1b8d611c84633ad07207
Grzegorz Urbański <wroblowski.grzegorz@gmail.com>
2024-06-25 13:47:07 +0200

    commit

--- USOSmobile/Pages/LoginPage.xaml.cs
+++ USOSmobile/Pages/LoginPage.xaml.cs
@@ -1,27 +1,35 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using USOSmobile.Models;
+using USOSmobile.SubPages;
 
 namespace USOSmobile
 {
-    
     public partial class LoginPage : ContentPage
     {
         public LoginPage()
         {
             InitializeComponent();
+            Task.Run(() =>
+            {
+                Login();
+            });
         }
 
-        private async void OnLoginBtnClicked(object sender, EventArgs e)
+
+        private async void Login()
         {
-            if ((await SecureStorage.GetAsync("oauth_token")) != null && (await SecureStorage.GetAsync("oauth_token_secret")) != null) {
-                Shell.    
-            }
-            else
+            if (((await SecureStorage.GetAsync("oauth_token")) == null || (await SecureStorage.GetAsync("oauth_token_secret")) == null) || !(await ModelObjects.apiBrowser.getUser()))
             {
                 await ModelObjects.apiBrowser.requestToken();
                 await ModelObjects.apiBrowser.PINAuthorization();
                 Shell.Current.GoToAsync($"//{nameof(PinPage)}");
             }
+            else
+            {
+                Shell.Current.GoToAsync($"//{nameof(MyUSOSPage)}");
+                await ModelObjects.apiBrowser.getCourses();
+            }
+
         }
     }
 }
